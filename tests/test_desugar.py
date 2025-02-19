43b7c05bc34bae672397dc08f185b502854dbaf7
James Clause <clause@udel.edu>
2025-02-19 13:00:51 -0500

    updates

--- tests/test_desugar.py
+++ tests/test_desugar.py
@@ -481,11 +481,7 @@ def test_desugar_expr_cond(
             ),
             (
                 NonDescending([Int(1), Int(2), Int(3)]),
-                If(
-                    GreaterThanOrEqualTo(Int(2), Int(1)),
-                    If(GreaterThanOrEqualTo(Int(3), Int(2)), Bool(True), Bool(False)),
-                    Bool(False),
-                ),
+                If(GreaterThanOrEqualTo(Int(2), Int(1)), GreaterThanOrEqualTo(Int(3), Int(2)), Bool(False)),
             ),
         ]
     ),
@@ -516,11 +512,7 @@ def test_desugar_expr_non_descending(
             ),
             (
                 Ascending([Int(1), Int(2), Int(3)]),
-                If(
-                    LessThan(Int(1), Int(2)),
-                    If(LessThan(Int(2), Int(3)), Bool(True), Bool(False)),
-                    Bool(False),
-                ),
+                If(LessThan(Int(1), Int(2)), LessThan(Int(2), Int(3)), Bool(False)),
             ),
         ]
     ),
@@ -551,11 +543,7 @@ def test_desugar_expr_ascending(
             ),
             (
                 Same([Int(1), Int(2), Int(3)]),
-                If(
-                    EqualTo(Int(1), Int(2)),
-                    If(EqualTo(Int(2), Int(3)), Bool(True), Bool(False)),
-                    Bool(False),
-                ),
+                If(EqualTo(Int(1), Int(2)), EqualTo(Int(2), Int(3)), Bool(False)),
             ),
         ]
     ),
@@ -586,11 +574,7 @@ def test_desugar_expr_same(
             ),
             (
                 Descending([Int(1), Int(2), Int(3)]),
-                If(
-                    LessThan(Int(2), Int(1)),
-                    If(LessThan(Int(3), Int(2)), Bool(True), Bool(False)),
-                    Bool(False),
-                ),
+                If(LessThan(Int(2), Int(1)), LessThan(Int(3), Int(2)), Bool(False)),
             ),
         ]
     ),
@@ -621,11 +605,7 @@ def test_desugar_expr_descending(
             ),
             (
                 NonAscending([Int(1), Int(2), Int(3)]),
-                If(
-                    GreaterThanOrEqualTo(Int(1), Int(2)),
-                    If(GreaterThanOrEqualTo(Int(2), Int(3)), Bool(True), Bool(False)),
-                    Bool(False),
-                ),
+                If(GreaterThanOrEqualTo(Int(1), Int(2)), GreaterThanOrEqualTo(Int(2), Int(3)), Bool(False)),
             ),
         ]
     ),
